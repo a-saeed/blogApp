9e94124790a83f2dc41d9127fad4210f8d77fae1
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-01-20 00:57:58 +0200

    encrypting user password using bcrypt

--- controllers/register.js
+++ controllers/register.js
@@ -1,14 +1,18 @@
 const router = require('express').Router()
 const User = require('../model/userModel');
+const bcrypt = require('bcrypt');
 
 //register new user to the system
 const registerUser = async (req, res) => {
     try {
+        //encrypt the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
         //create a new user
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPassword
         })
         //save the new user to the DB
         const user = await newUser.save()
