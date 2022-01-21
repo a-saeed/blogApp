f9d43363c318be301421c80bda0e7c847e4be1c5
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-01-21 21:58:35 +0200

    providing /logout route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 const authRouter = require('express').Router()
 const register = require('../controllers/register');
 const login = require('../controllers/login');
+const logout = require('../controllers/logout');
+const checkAuthentication = require('../middlewares/checkAuthentication');
 
 authRouter.post("/register", register)
 authRouter.post("/login", login)
+authRouter.post("/logout",checkAuthentication, logout)
 
-module.exports = authRouter, login
+module.exports = authRouter
