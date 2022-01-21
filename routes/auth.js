5716e6478547cfe64622475b4109d896e9011993
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-01-21 19:20:22 +0200

    providing /login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const authRouter = require('express').Router()
 const register = require('../controllers/register');
+const login = require('../controllers/login');
 
 authRouter.post("/register", register)
+authRouter.post("/login", login)
 
-module.exports = authRouter
+module.exports = authRouter, login
