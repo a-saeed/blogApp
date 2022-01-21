f78af01786e602374fb198d80185f01337392c95
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-01-21 16:29:16 +0200

    on register, attach a new token to cookie

--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const User = require('../model/userModel');
 const bcrypt = require('bcrypt');
 const CustomError = require('../model/customError');
+const Token = require('../model/token');
 
 //register new user to the system
 const registerUser = async (req, res, next) => {
@@ -17,6 +18,12 @@ const registerUser = async (req, res, next) => {
         })
         //save the new user to the DB
         const user = await newUser.save()
+        //generate a token for new logged in user and save it in db
+        const token = new Token({ userId: user._id })
+        const newToken = await token.save()
+        //set cookie with the newly generated token
+        res.cookie('session', newToken._id);
+
         res.status(200).json(user)
     } catch (err) {
         next(new CustomError(500, "a problem happened while registering... " + err.message))
