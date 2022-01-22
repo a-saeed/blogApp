ee79282098e6c0130e1887f493e3dbed08344a32
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-01-22 23:38:18 +0200

    providing (delete & get) routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
-const updateUser = require('../controllers/userController')
+const { updateUser, deleteUser, getCurrentUser} = require('../controllers/userController')
 const checkAuthentication = require('../middlewares/checkAuthentication')
 
 const userRouter = require('express').Router()
 
-//update authenticated users only
+//(update-delete-get) authenticated users only
 userRouter.put('/:id', checkAuthentication, updateUser)
+userRouter.delete('/:id', checkAuthentication, deleteUser)
+userRouter.get('/', checkAuthentication, getCurrentUser)
 
 module.exports  = userRouter
