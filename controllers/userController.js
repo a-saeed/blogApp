c76fc1130ccebcfbebe27d905974a880c73cc73a
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-01-22 23:37:26 +0200

    implementing Delete/Get current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const userModel = require("../model/userModel")
 const bcrypt = require('bcrypt');
 const CustomError = require("../model/customError");
 const Token = require("../model/token");
+const postModel = require("../model/postModel");
 
 //update user info 
 const updateUser = async (req, res, next) => {
@@ -18,4 +19,28 @@ const updateUser = async (req, res, next) => {
     }
 }
 
-module.exports = updateUser
+//delete user
+const deleteUser = async (req, res, next) => {
+    try {
+        //req object already has user info from checkAuth
+        //delete all user posts
+        //then delete user
+        await postModel.deleteMany({ username: req.user.username }).catch(err => { res.statusCode = 500; throw "server error"})
+        await userModel.findByIdAndDelete(req.params.id).catch(err => { res.statusCode = 500; throw "server error"});
+        res.status(200).json("user deleted")
+    } catch (err) {
+        next(new CustomError(res.statusCode, err));
+    }
+}
+
+//get current logged in user
+const getCurrentUser = async (req, res, next) => {
+    try {
+        const user = userModel.findOne({ _id: req.user._id }).catch(err => { res.statusCode = 500; throw "server error"})
+        res.status(200).json(req.user)
+    } catch (err) {
+        next(new CustomError(res.statusCode, err));
+    }
+}
+
+module.exports = { updateUser, deleteUser, getCurrentUser }
